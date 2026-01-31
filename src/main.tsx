c972f8c5a2ad0349ec3d27ffc884696ef6d6c209
hi991016 <miureus@gmail.com>
2026-01-31 16:14:58 +0700

    done

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+// import { preconnectFonts, loadFonts } from 'src/utils/LoadFonts'
 import App from './App.tsx'
 
 import 'src/styles/main.scss'
+
+// load fonts
+// preconnectFonts()
+// setTimeout(() => {
+//   loadFonts()
+// }, 0)
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
